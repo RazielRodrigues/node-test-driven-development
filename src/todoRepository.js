f94d4d4f18911d4d8b9925819550fcac392dd301
Raziel Rodrigues <razielx3@live.com>
2022-08-09 00:22:44 -0300

    todo repository

---
+++ src/todoRepository.js
@@ -0,0 +1,19 @@
+const loki = require('lokijs');
+
+class TodoRepository {
+    constructor() {
+        const db = new loki('todo', {});
+        this.schedule = db.addCollection('schedule');
+    }
+
+    list() {
+        return this.schedule.find()
+    }
+
+    create(data) {
+        return this.schedule.insertOne(data)
+    }
+
+}
+
+module.exports = TodoRepository;
